e8c98c4b188fc5b6c9be963ce21ecc2d9a444d9c
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-02-19 17:15:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="app">
+
+        <el-row :gutter="10">
+            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            startHacking() {
+                this.$notify({
+                    title: 'It works!',
+                    type: 'success',
+                    message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+                    duration: 5000
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Helvetica, sans-serif;
+        text-align: center;
+    }
+    .el-col {
+        border-radius: 4px;
+    }
+    .bg-purple-dark {
+        background: #99a9bf;
+    }
+    .bg-purple {
+        background: #d3dce6;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+</style>
